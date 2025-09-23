dbe355f70361c381070d7264a032d9141b326874
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-09-22 22:21:54 -0600

    Reminders endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import Router from "express";
 import auth from "./auth";
 import mascotas from "./mascotas";
 import avatares from "./avatares";
+import reminders from "./reminders";
 
 const router = Router();
 
 router.use("/auth", auth);
 router.use("/mascotas", mascotas);
 router.use("/avatares", avatares);
+router.use("/reminders", reminders)
 
 export default router;

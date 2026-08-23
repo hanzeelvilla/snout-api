bd7533c327da4365aea282c978620c123cfcd96d
hanzeelvilla <hanzeelvilla0@gmail.com>
2026-08-23 11:45:17 -0600

    Prettier and Eslint setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from './database/data-source';
-import { AuthModule } from './auth/auth.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { dataSourceOptions } from "./database/data-source";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
